0306d1bd4b61578bcba834fdbd1a7ccd4d8b6c5a
Renan Dias <renansdiaz@gmail.com>
2018-10-02 07:14:01 -0400

    Added files for Nodejs API

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const config = require('./config/config.json');
+
+var mysql = require('mysql');
+    var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : config.db_password,
+    database : 'USERS',
+    multipleStatements: true
+});
+
+connection.connect();
+
+app.get('/user/:id', function(req, res) {
+    const query = `SELECT username FROM users where id = ${req.params.id};`;
+    console.log(query);
+
+    connection.query(query, function (error, results, fields) {
+        if (error) throw error;
+
+        console.log(results);
+
+        let arr = [];
+        results.forEach(e => {
+            console.log(e);
+
+            if (e instanceof Object && 'username' in e) {
+                arr.push(e.username);
+            } else if (e instanceof Array && 'username' in e[0]) {
+                arr.push(e[0].username);
+            }
+        });
+
+        console.log(arr);
+
+        res.send(arr);
+    });
+});
+
+app.listen(3000, function() {
+    console.log('Server is running...');
+});
